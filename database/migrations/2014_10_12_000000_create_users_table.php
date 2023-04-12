98e3f710380c93b5d98c2a878e4fb32c7ddd2058
mehrab kb <mehrabkordbacheh@yahoo.com>
2023-04-12 11:46:42 +0430

    first commit with first migrations

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->unsignedBigInteger('user_role_id');
+            $table->string('avatar');
+            $table->smallInteger('score');
+            $table->string('mobile' , 12);
+            $table->string('national_code' , 10);
+            $table->integer('verify_code');
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
